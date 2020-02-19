e75f87f765391f12753ea186ba37b4d79f4f178b
Tejas <tejas.shekokar@velotio.com>
2020-02-20 02:58:19 +0530

    update app.py

--- circle-ci-webhook/app.py
+++ circle-ci-webhook/app.py
@@ -9,17 +9,25 @@ from os.path import join, dirname
 
 app = Flask(__name__)
 if not os.environ.get('CIRCLECI_TOKEN'):
+    print('CIRCLECI_TOKEN not found')
     sys.exit()
 webhook = Webhook(app, endpoint='/postreceive')
 Repos = ['tejasshekokar/learn-express']
 # Branch = 'refs/heads/master'
 Branch = 'refs/heads/staging'
+MASTER_BRANCH = 'refs/heads/master'
 @webhook.hook(event_type='push')
 def on_pull_request(data):
     print('Lamda Tiggered')
+    param=''
+    if MASTER_BRANCH==data['ref']:
+        param = {'run_tests': 'true','smoke_tests':'false'}
+    else:
+        param = {'run_tests':'false','smoke_tests':'true'}
     payload = {
         'repo': data['repository']['full_name'],
-        'branch': data['ref']
+        'branch': data['ref'],
+        'param': param
     }
     print('Payload: %s' % (payload))
     source_event = {'type': 'push', 'branch': Branch}
@@ -27,15 +35,16 @@ def on_pull_request(data):
     #     circleci_new_build(source_event, payload)                                          #only build master branch cnfig
     if payload['repo'] in Repos and payload['branch'] != source_event['branch']:
         circleci_new_build(source_event, payload)
-
 def circleci_new_build(source_event, payload):
     worker_token = os.environ.get('CIRCLECI_TOKEN')
     worker_repo = payload['repo']
     worker_branch = payload['branch'].replace("refs/heads/", "")
+    worker_param_one = payload['param']['run_tests']
+    worker_param_two = payload['param']['smoke_tests']
     headers = {'Content-Type': 'application/json',
                'Circle-Token': '%s' % worker_token}
     # data = '{"branch": "master"}'            #It will trigger only master branch job
-    data = '{"branch": "%s"}' % (worker_branch)
+    data = '{"branch": "%s","parameters": {"run_tests": %s,"smoke_tests":%s} }' % (worker_branch,worker_param_one,worker_param_two)
     api_url = 'https://circleci.com/api/v2/project/gh/' + worker_repo + '/pipeline'
     try:
         response = requests.post(
@@ -46,4 +55,5 @@ def circleci_new_build(source_event, payload):
         print(ex.message)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    print('app is running')
+    app.run(host="0.0.0.0", port="5000",debug=True)
