25e277268d4c27d36dbe2aae4fc202d68658bd63
Tejas <tejas.shekokar@velotio.com>
2020-02-19 14:44:57 +0530

    build all commits for master branch

--- circle-ci-webhook/app.py
+++ circle-ci-webhook/app.py
@@ -20,21 +20,17 @@ if not os.environ.get('CIRCLECI_TOKEN'):
     sys.exit()
 webhook = Webhook(app, endpoint='/postreceive')
 Repos = ['tejasshekokar/learn-express']
-Branch = ['master']
-Branch_pattern = ['mase-']
-@webhook.hook(event_type='pull_request')
+Branch = ['refs/heads/master']
+@webhook.hook(event_type='push')
 def on_pull_request(data):
     print('Inside on_pull_request')
     print(data)
     payload = {
-        'repo': data['pull_request']['head']['repo']['full_name'],
-        'branch': data['pull_request']['head']['ref'],
-        'tag': '',
-        'revision': data['pull_request']['head']['sha']
+        'repo': data['repository']['full_name'],
+        'branch': data['ref']
     }
-    source_event = {'type': 'pull_request',
-                    'action': data['action'], 'number': data['number']}
-    if payload['repo'] in Repos and source_event['action'] == "opened" or source_event['action'] == "reopened":
+    source_event = {'type': 'push', 'branch': Branch}
+    if payload['repo'] in Repos and payload['branch'] in source_event['branch']:
         circleci_new_build(source_event, payload)
 
 
@@ -44,20 +40,21 @@ def circleci_new_build(source_event, payload):
     print(payload)
     worker_token = os.environ.get('CIRCLECI_TOKEN')
     worker_repo = payload['repo']
-    worker_branch = payload['branch']
     print(worker_token)
-    headers = {'Content-Type': 'application/json', 'Circle-Token':'%s' % worker_token}
-    data = '{"branch": "%s"}' % (worker_branch)
+    headers = {'Content-Type': 'application/json',
+               'Circle-Token': '%s' % worker_token}
+    data = '{"branch": "master"}'
+    # data = '{"branch": "%s"}' % (worker_branch)
     api_url = 'https://circleci.com/api/v2/project/gh/' + worker_repo + '/pipeline'
     print(api_url)
     try:
         response = requests.post(
             api_url, headers=headers, data=data)
         print(response.status_code)
-        return 
+        return
     except Exception as ex:
         print(ex.message)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000",debug=True)
+    app.run(host="0.0.0.0", port="5000", debug=True)
