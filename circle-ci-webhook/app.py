0bfeaec55ee0e7222c277d63870de0f3ea6a0525
Tejas <tejas.shekokar@velotio.com>
2020-02-19 16:39:32 +0530

    final setup

--- circle-ci-webhook/app.py
+++ circle-ci-webhook/app.py
@@ -5,26 +5,16 @@ import sys
 from ipaddress import ip_address, ip_network
 from functools import wraps
 import requests
-from dotenv import load_dotenv
 from os.path import join, dirname
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
 app = Flask(__name__)
-print('Inside app.py')
-print(os.environ.get('CIRCLECI_TOKEN'))
 if not os.environ.get('CIRCLECI_TOKEN'):
-    print('Inside app.py')
-    print('CIRCLECI_TOKEN is not set')
     sys.exit()
 webhook = Webhook(app, endpoint='/postreceive')
 Repos = ['tejasshekokar/learn-express']
 Branch = ['refs/heads/master']
 @webhook.hook(event_type='push')
 def on_pull_request(data):
-    print('Inside on_pull_request')
-    print(data)
     payload = {
         'repo': data['repository']['full_name'],
         'branch': data['ref']
@@ -35,18 +25,13 @@ def on_pull_request(data):
 
 
 def circleci_new_build(source_event, payload):
-    print('Inside circleci_new_build')
-    print('Payload\n')
-    print(payload)
     worker_token = os.environ.get('CIRCLECI_TOKEN')
     worker_repo = payload['repo']
-    print(worker_token)
     headers = {'Content-Type': 'application/json',
                'Circle-Token': '%s' % worker_token}
     data = '{"branch": "master"}'
     # data = '{"branch": "%s"}' % (worker_branch)
     api_url = 'https://circleci.com/api/v2/project/gh/' + worker_repo + '/pipeline'
-    print(api_url)
     try:
         response = requests.post(
             api_url, headers=headers, data=data)
